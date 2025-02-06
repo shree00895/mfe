02a6cc40ccd0194c19284d278ea80b727e448fa0
shree <shreejeetjkventures@gmail.com>
2025-02-06 16:12:47 +0530

    Added console

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
+    console.log("hey");
     return <>
         <h1>Hey there!</h1>
         <hr />
