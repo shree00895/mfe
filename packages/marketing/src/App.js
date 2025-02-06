e4149fa228697909409ef863b8086b139dfff650
shree <shreejeetjkventures@gmail.com>
2025-02-06 19:04:26 +0530

    updated dproduction domain in github secret

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -6,7 +6,7 @@ import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
 export default () => {
-    console.log('hi');
+   
     return (
         <StylesProvider>
             <BrowserRouter>
